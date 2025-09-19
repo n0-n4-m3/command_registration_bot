b2b9a6a4b3f226deee9534d267d8e2a28997a3c1
n0-n4-m3 <556sueubaev@mail.ru>
2025-09-19 06:37:19 +0300

    lol

--- scenes.py
+++ scenes.py
@@ -35,7 +35,6 @@ class QuizScene(Scene, state="quiz"):
     async def on_enter(self, message: Message, state: FSMContext, step: int | None = 0) -> Any:
         if not step:
             await message.answer(que_json["start_text"])
-            await message.answer(que_json["start_text"])
         
         try:
             if step != len(QUESTIONS) - 1:
@@ -68,7 +67,6 @@ class QuizScene(Scene, state="quiz"):
             )
         except:
             print("boyya")
-            print("boyya")
 
     @on.poll_answer.exit()
     async def on_exit_poll(self, poll_answer: PollAnswer, state: FSMContext) -> None:
@@ -83,20 +81,7 @@ class QuizScene(Scene, state="quiz"):
         await poll_answer.bot.send_message(chat_id=poll_answer.user.id, text=text, reply_markup=ReplyKeyboardRemove()) # type: ignore
         await state.set_data({})
         
-    async def on_exit_poll(self, poll_answer: PollAnswer, state: FSMContext) -> None:
-        data = await state.get_data()
-        answers = data.get("answers", {})
-        questionnaire = zip([x.text for x in QUESTIONS], answers.values())
-        
-        text = ""
-        for x,y in questionnaire:
-            text = text + f"{x}: {y}\n"
-
-        await poll_answer.bot.send_message(chat_id=poll_answer.user.id, text=text, reply_markup=ReplyKeyboardRemove()) # type: ignore
-        await state.set_data({})
-        
     @on.message.exit()
-    async def on_exit(self, message: Message, state: FSMContext) -> None:
     async def on_exit(self, message: Message, state: FSMContext) -> None:
         data = await state.get_data()
         answers = data.get("answers", {})
@@ -172,13 +157,10 @@ class QuizScene(Scene, state="quiz"):
 
 quiz_router = Router(name=__name__)
 quiz_router.message.register(QuizScene.as_handler(), Command("start"))
-quiz_router.message.register(QuizScene.as_handler(), Command("start"))
 
 def create_dispatcher():
     dispatcher = Dispatcher(events_isolation=SimpleEventIsolation())
-    dispatcher = Dispatcher(events_isolation=SimpleEventIsolation())
     dispatcher.include_router(quiz_router)
-    
     
     scene_registry = SceneRegistry(dispatcher)
     scene_registry.add(QuizScene)
